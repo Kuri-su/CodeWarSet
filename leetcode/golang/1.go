7fa87d44a34b10c40f41af11370635d5ac14f5fc
Amatist_Kurisu <misaki.zhcy@gmail.com>
2019-02-17 19:22:46 +0800

    finish 38

--- leetcode/golang/1.go
+++ leetcode/golang/1.go
@@ -1,6 +1,27 @@
 package golang
 
 func TwoSum(nums []int, target int) []int {
+	for k1, v1 := range nums {
+		for k2, v2 := range nums {
+			if k1 != k2 && v1+v2 == target {
+				return []int{k1, k2}
+			}
+		}
+	}
+	return nil
+}
 
+// Once hash table
 
+func TwoSum2(nums []int, target int) []int {
+	m := make(map[int]int)
+	for index, value := range nums {
+		temp := target - value
+		if i, ok := m[temp]; ok && m[temp] != index {
+			return []int{i, index}
+		}
+		// 键值调转排列
+		m[value] = index
+	}
+	return nil
 }
---
+++ leetcode/golang/38.go
@@ -0,0 +1,66 @@
+package golang
+
+import (
+	`bytes`
+	`fmt`
+	`strconv`
+)
+
+func CountAndSay(n int) string {
+	res := "1"
+
+	var (
+		temp   = ""
+		number = 0
+	)
+
+	for i := 2; i <= n; i++ {
+		lastStr := string(res[0])
+
+		for k, v := range res {
+			str := string(v)
+			if str == lastStr {
+				number++
+			} else {
+				temp += fmt.Sprintf("%d", number) + lastStr
+				number = 1
+				lastStr = str
+			}
+
+			if k == len(res)-1 {
+				temp += fmt.Sprintf("%d", number) + lastStr
+			}
+		}
+		lastStr = ""
+		number = 0
+		res = temp
+		temp = ""
+	}
+
+	return res
+}
+
+
+func countAndSay2(n int) string {
+	if n == 1 {
+		return "1"
+	}
+	lastStr := countAndSay2(n - 1)
+	res := bytes.NewBufferString("")
+	curNum := lastStr[0]
+	curCoefficient := 1
+	lastStrLen := len(lastStr)
+	for i := 1; i < lastStrLen; i++ {
+		if lastStr[i] == curNum {
+			curCoefficient += 1
+		} else {
+			res.WriteString(strconv.Itoa(curCoefficient))
+			res.WriteString(string(curNum))
+			curNum = lastStr[i]
+			curCoefficient = 1
+		}
+	}
+	res.WriteString(strconv.Itoa(curCoefficient))
+	res.WriteString(string(curNum))
+	return res.String()
+}
