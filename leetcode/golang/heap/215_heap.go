4c8bce44430997783fce591ab2d00a8f973e9dd3
Amatist Kurisu <misaki.zhcy@gmail.com>
2019-08-20 00:12:56 +0800

    feat: update problems

--- leetcode/golang/heap/215_heap.go
+++ leetcode/golang/heap/215_heap.go
@@ -6,15 +6,24 @@ import "container/heap"
    利用 堆 处理此问题, 极简
 
    解题思想:
+		所谓栈的 POP 实际上是把最大的元素挪到最后, 逐步修复栈
 
-   时间复杂度: O(logN)
-   空间复杂度: O(n) (可优化)
+   时间复杂度: O(n)
+   空间复杂度: O(1)
 
    过程:
 */
 
 func findKthLargest2(nums []int, k int) int {
-
+	var res int
+	n := IntHeap(nums)
+	heap.Init(n)
+	for i := 0; i < k; i++ {
+		heap.Pop(n)
+		res = n[len(n)-1]
+		n = n[:len(n)-1]
+	}
+	return res
 }
 
 type IntHeap []int
@@ -24,7 +33,7 @@ func (h IntHeap) Len() int {
 }
 
 func (h IntHeap) Less(i, j int) bool {
-	return i < j
+	return h[i] > h[j]
 }
 
 func (h IntHeap) Swap(i, j int) {
@@ -32,7 +41,6 @@ func (h IntHeap) Swap(i, j int) {
 }
 
 func (h IntHeap) Push(x interface{}) {
-
 }
 
 func (h IntHeap) Pop() interface{} {
